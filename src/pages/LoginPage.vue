c4db9609a2bd40bfc7ab1770da9234f6aafe1c7e
20jun01 <10junjun20@gmail.com>
2022-05-24 17:16:50 +0900

    create city link

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,25 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+// import Login from "../components/Login.vue";
+const user_name = ref("");
+const password = ref("");
+const sendUser = () => {
+  axios.post("/api/login", {
+    username: user_name.value,
+    password: password.value,
+  });
+};
+</script>
+
+<template>
+  <div class="login">
+    <div>
+      <input v-model="user_name" type="text" />
+      <input v-model="password" type="password" />
+    </div>
+    <div>
+      <button @click="sendUser">送信</button>
+    </div>
+  </div>
+</template>
